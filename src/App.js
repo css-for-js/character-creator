b00c54650912983b925212f979a8a8fd4c2764d0
Joshua Comeau <joshwcomeau@gmail.com>
2020-12-10 12:16:42 -0500

    Proper initial stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Body01 from './assets/bodies/01';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Body01
+        skinColor="red"
+        clothesColor="green"
+        style={{ width: 250 }}
+      />
     </div>
   );
 }
