059e5b3e118972aa6dd5ad0d9196b42cd30cf9f3
Joshua Comeau <joshwcomeau@gmail.com>
2021-01-04 11:32:41 -0500

    Exercise 4 solution

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,6 +30,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveEffect}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -88,6 +89,7 @@ function App() {
           clothesColor={clothesColor}
         />
       </div>
+
     </main>
   );
 }
