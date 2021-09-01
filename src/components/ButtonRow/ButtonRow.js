1e9834592db144badb61ad615ff0e475663680a5
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-01 08:33:01 +0800

    Delete src directory

--- src/components/ButtonRow/ButtonRow.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './ButtonRow.module.css';
-
-const ButtonRow = ({ children }) => {
-  return <div className={styles.buttonRow}>{children}</div>;
-};
-
-export default ButtonRow;
