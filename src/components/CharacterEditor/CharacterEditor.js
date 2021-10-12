b809b2281a5409dc94c63d1eb7783232bf73b982
Nacho <ikornzaft@gmail.com>
2021-10-12 12:02:25 -0300

    Nacho's solution

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,6 +30,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveBar} />
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
