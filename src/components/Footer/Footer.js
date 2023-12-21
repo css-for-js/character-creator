87e034450b4985afe31ed7d73bf5252c8ab8ea49
phebs <phebs@github.com>
2023-12-21 22:44:15 +1100

    updated the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,6 +10,7 @@ const Footer = () => {
         href="https://www.openpeeps.com/"
         target="_blank"
         rel="noopener noreferrer"
+        className={styles.a}
       >
         Open Peeps
       </a>
